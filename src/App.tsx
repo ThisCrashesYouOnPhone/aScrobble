a366abb220809e45daaa7355e982fba1d2f7d4cb
a a <raiexists@gmail.com>
2026-08-22 20:18:14 -0600

    chore: bump version to 1.1.2

--- src/App.tsx
+++ src/App.tsx
@@ -312,7 +312,7 @@ export default function App() {
       </main>
 
       <footer className="app-footer">
-        <span>aScrobble v1.1.1</span>
+        <span>aScrobble v1.1.2</span>
         <div className="footer-links">
           <a href="https://github.com/ThisCrashesYouOnPhone/aScrobble" target="_blank" rel="noreferrer">
             GitHub
